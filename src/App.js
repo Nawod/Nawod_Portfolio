de887d383dac8a5078cbd955ddbac1b694d0a66d
Nawod <madhuvanthanawo1@gmail.com>
2021-11-27 23:39:25 +0530

    add scroll bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
+import SmoothScrollBar from './components/SmoothScrollBar';
 import About from './page/About';
 import Contact from './page/Contact';
 import Home from './page/Home';
@@ -11,20 +12,22 @@ export default function App() {
     <div>
       <Router>
         <NavMenu />
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <SmoothScrollBar>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </SmoothScrollBar>
       </Router>
     </div>
   );
